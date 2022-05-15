37cac76435022686ca8def483788c93696fa0d1c
tankbottoms <m@tankbottoms.xyz>
2022-05-15 04:10:55 -0400

    redoing graphic assets

---
+++ bannyverse-resources/build/_app/pages/index.svelte-9ac79bc9.js
@@ -0,0 +1,166 @@
+import{L as K,S as U,i as Y,s as q,k as M,e as I,K as $,d as h,m as x,c as k,a as v,b as L,g as b,E as f,M as X,N as e0,O as D,t as S,h as H,P as t0,J as w,Q as j,R as E,T as V,U as Q,l as G,V as a0,W as l0,X as W,j as n0}from"../chunks/index-f52b3be9.js";import{w as o0}from"../chunks/index-9be6da39.js";async function N(t){let e=`<svg
+		width="300"
+		height="300"
+		viewBox="0 0 290 290"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+	>
+		<defs>
+			<style>
+				@import url('https://gateway.pinata.cloud/ipfs/QmRodGNTG8Jex8nQQwufuNi4Brb4Cqy16YBJ3CKqBYfQKP/DM_Mono.css');
+			</style>
+			<filter id="f1">
+				<feImage
+					result="p0"
+					xlink:href="data:image/svg+xml;base64,CiAgICAgICAgICA8c3ZnIHdpZHRoPScyOTAnIGhlaWdodD0nMjkwJyB2aWV3Qm94PScwIDAgMjkwIDI5MCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz4KICAgICAgICAgICAgPHJlY3Qgd2lkdGg9JzI5MHB4JyBoZWlnaHQ9JzI5MHB4JyBmaWxsPScjMTIzNDU2Jy8+CiAgICAgICAgICA8L3N2Zz4KICAgICAgICAgIA=="
+				/>
+				<feImage
+					result="p1"
+					xlink:href="data:image/svg+xml;base64,CiAgICAgICAgICA8c3ZnIHdpZHRoPScyOTAnIGhlaWdodD0nMjkwJyB2aWV3Qm94PScwIDAgMjkwIDI5MCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz4KICAgICAgICAgICAgPGNpcmNsZSBjeD0nMTkyJyBjeT0nMzYyJyByPScxMjBweCcgZmlsbD0nI2FiY2RlYScvPgogICAgICAgICAgPC9zdmc+CiAgICAgICAgICA="
+				/>
+				<feImage
+					result="p2"
+					xlink:href="data:image/svg+xml;base64,CiAgICAgICAgICA8c3ZnIHdpZHRoPScyOTAnIGhlaWdodD0nMjkwJyB2aWV3Qm94PScwIDAgMjkwIDI5MCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz4KICAgICAgICAgICAgPGNpcmNsZSBjeD0nMjQzJyBjeT0nMzYyJyByPScxMjBweCcgZmlsbD0nIzY3ODkwMScvPgogICAgICAgICAgPC9zdmc+CiAgICAgICAgICA="
+				/>
+				<feImage
+					result="p3"
+					xlink:href="data:image/svg+xml;base64,CiAgICAgICAgICA8c3ZnIHdpZHRoPScyOTAnIGhlaWdodD0nMjkwJyB2aWV3Qm94PScwIDAgMjkwIDI5MCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz4KICAgICAgICAgICAgPGNpcmNsZSBjeD0nMzcnIGN5PScxMzEnIHI9JzEwMHB4JyBmaWxsPScjZmFiY2RmJy8+CiAgICAgICAgICA8L3N2Zz4KICAgICAgICAgIA=="
+				/>
+				<feBlend mode="overlay" in="p0" in2="p1" />
+				<feBlend mode="exclusion" in2="p2" />
+				<feBlend mode="overlay" in2="p3" result="blendOut" />
+				<feGaussianBlur in="blendOut" stdDeviation="42" />
+			</filter>
+			<clipPath id="corners">
+				<rect width="290" height="290" rx="42" ry="42" />
+			</clipPath>',
+			<path
+				id="text-path-a"
+				d="M40 12 H250 A28 28 0 0 1 278 40 V250 A28 28 0 0 1 250 278 H40 A28 28 0 0 1 12 250 V40 A28 28 0 0 1 40 12 z"
+			/>
+			<path id="minimap" d="M234 444C234 457.949 242.21 463 253 463" />
+			<filter id="top-region-blur">
+				<feGaussianBlur in="SourceGraphic" stdDeviation="24" />
+			</filter>
+			<linearGradient id="grad-up" x1="1" x2="0" y1="1" y2="0">
+				<stop offset="0.0" stop-color="white" stop-opacity="1" />
+				<stop offset=".9" stop-color="white" stop-opacity="0" />
+			</linearGradient>
+			<linearGradient id="grad-down" x1="0" x2="1" y1="0" y2="1">
+				<stop offset="0.0" stop-color="white" stop-opacity="1" />
+				<stop offset="0.9" stop-color="white" stop-opacity="0" />
+			</linearGradient>
+			<mask id="fade-up" maskContentUnits="objectBoundingBox">
+				<rect width="1" height="1" fill="url(#grad-up)" />
+			</mask>
+			<mask id="fade-down" maskContentUnits="objectBoundingBox">
+				<rect width="1" height="1" fill="url(#grad-down)" />
+			</mask>
+			<mask id="none" maskContentUnits="objectBoundingBox">
+				<rect width="1" height="1" fill="white" />
+			</mask>
+			<linearGradient id="grad-symbol">
+				<stop offset="0.7" stop-color="white" stop-opacity="1" />
+				<stop offset=".95" stop-color="white" stop-opacity="0" />
+			</linearGradient>
+			<mask id="fade-symbol" maskContentUnits="userSpaceOnUse">
+				<rect width="290px" height="200px" fill="url(#grad-symbol)" />
+			</mask>
+		</defs>
+		<g clip-path="url(#corners)">
+			<rect fill="123456" x="0px" y="0px" width="290px" height="290px" />
+			<rect style="filter: url(#f1)" x="0px" y="0px" width="290px" height="290px" />
+			<g style="filter:url(#top-region-blur); transform:scale(1.5); transform-origin:center top;">
+				<rect fill="none" x="0px" y="0px" width="290px" height="290px" />
+				<ellipse cx="50%" cy="0px" rx="180px" ry="120px" fill="#000" opacity="0.85" /></g
+			>
+			<rect
+				x="0"
+				y="0"
+				width="290"
+				height="290"
+				rx="42"
+				ry="42"
+				fill="rgba(0,0,0,0)"
+				stroke="rgba(255,255,255,0.2)"
+			/>
+		</g>
+
+		<rect
+			x="16"
+			y="16"
+			width="258"
+			height="258"
+			rx="26"
+			ry="26"
+			fill="rgba(0,0,0,0)"
+			stroke="rgba(255,255,255,0.2)"
+		/>
+        `;for(const[a,n]of Object.entries(t)){if(!n)continue;const s=`/veBanny/${a}/${n}.png`,o=await fetch(s);var l=new FileReader;l.readAsDataURL(await o.blob()),await new Promise(i=>{l.onloadend=function(){var d=l.result;e+=`<image
+				        xlink:href="${d}"
+				        x="50%"
+				        y="50%"
+				        width="250"
+				        style="transform: translate(-125px, -125px);"
+				    />`,i(!0)}})}return e+=`
+		<path
+			style="transform:translate(226px, 226px) scale(0.1)"
+			id="Selection"
+			fill="white"
+			d="M 146.00,64.00 C 153.56,65.52 160.73,67.80 168.00,70.34 171.38,71.53 176.09,73.24 178.26,76.21 180.53,79.32 180.99,89.94 181.00,94.00 181.00,94.00 181.00,162.00 181.00,162.00 181.00,162.00 180.00,177.00 180.00,177.00 180.00,177.00 180.00,206.00 180.00,206.00 180.00,206.00 178.96,223.00 178.96,223.00 178.96,223.00 178.96,239.00 178.96,239.00 178.96,239.00 178.00,249.00 178.00,249.00 177.95,253.95 177.94,265.83 175.83,270.00 172.97,275.62 162.77,281.04 157.00,283.40 138.16,291.09 122.85,291.23 103.00,291.00 86.28,290.80 51.09,282.65 34.00,278.37 28.20,276.92 11.05,272.45 7.31,268.61 4.73,265.96 4.48,261.52 4.00,258.00 4.00,258.00 1.58,236.00 1.58,236.00 1.58,236.00 0.91,224.00 0.91,224.00 0.91,224.00 0.00,212.00 0.00,212.00 0.00,212.00 0.00,147.00 0.00,147.00 0.00,147.00 1.00,132.00 1.00,132.00 1.00,132.00 3.91,88.00 3.91,88.00 4.19,84.21 4.47,73.25 6.74,70.63 9.03,67.98 22.09,62.96 26.00,61.40 34.98,57.81 60.95,50.19 70.00,50.18 70.00,50.18 88.00,52.59 88.00,52.59 88.00,52.59 115.00,57.20 115.00,57.20 117.47,57.67 123.43,59.14 125.57,57.89 128.38,56.25 130.28,45.40 131.13,42.00 131.13,42.00 136.58,20.00 136.58,20.00 138.28,12.35 139.13,5.41 147.00,1.45 150.40,-0.25 154.30,-0.04 158.00,0.00 165.96,0.11 172.77,4.01 180.00,6.99 180.00,6.99 216.00,22.22 216.00,22.22 223.21,25.40 233.61,27.26 228.91,38.00 224.21,48.76 216.65,43.52 209.00,40.14 209.00,40.14 174.00,24.70 174.00,24.70 171.62,23.62 162.67,19.02 160.59,19.58 156.57,20.66 155.23,27.54 154.37,31.00 154.37,31.00 146.00,64.00 146.00,64.00 Z M 124.00,69.00 C 124.00,69.00 72.00,60.32 72.00,60.32 67.35,59.97 54.19,62.85 50.00,65.00 50.00,65.00 112.00,79.87 112.00,79.87 117.41,81.23 133.47,85.93 138.00,85.45 142.55,84.96 157.13,80.37 161.00,78.00 161.00,78.00 146.14,74.61 146.14,74.61 142.67,75.03 143.18,78.25 138.94,80.55 134.35,83.03 125.51,81.82 123.46,76.56 122.80,74.88 123.73,70.92 124.00,69.00 Z M 124.00,88.00 C 124.00,88.00 59.00,72.35 59.00,72.35 45.30,69.20 36.90,64.66 24.00,74.00 24.00,74.00 95.00,88.58 95.00,88.58 104.21,90.24 115.96,94.45 124.00,88.00 Z M 109.00,102.00 C 109.00,102.00 44.00,88.58 44.00,88.58 44.00,88.58 14.00,82.00 14.00,82.00 14.00,82.00 11.00,130.00 11.00,130.00 11.00,130.00 10.00,147.00 10.00,147.00 10.00,147.00 10.00,213.00 10.00,213.00 10.00,213.00 10.91,223.00 10.91,223.00 10.91,223.00 12.72,247.00 12.72,247.00 13.11,250.03 13.71,258.36 15.17,260.61 17.65,264.42 34.07,268.10 39.00,269.37 39.00,269.37 62.00,274.65 62.00,274.65 65.99,275.55 73.09,277.25 77.00,276.66 86.29,275.25 93.68,266.96 97.73,259.00 105.49,243.74 109.97,213.23 110.00,196.00 110.00,196.00 110.00,136.00 110.00,136.00 110.00,136.00 109.00,121.00 109.00,121.00 109.00,121.00 109.00,102.00 109.00,102.00 Z M 165.00,88.00 C 165.00,88.00 151.00,93.00 151.00,93.00 156.84,99.26 153.13,108.76 156.00,116.00 156.00,116.00 165.00,88.00 165.00,88.00 Z M 150.00,93.00 C 144.50,95.21 145.98,99.76 146.00,105.00 146.00,105.00 147.00,126.00 147.00,126.00 152.14,125.14 152.71,123.91 154.00,119.00 152.15,118.54 151.21,118.63 150.02,116.77 148.78,114.83 149.03,111.26 149.00,109.00 148.89,101.12 146.78,100.63 150.00,93.00 Z M 138.00,97.00 C 138.00,97.00 125.00,100.00 125.00,100.00 127.71,105.74 132.89,110.34 138.00,114.00 138.00,114.00 138.00,97.00 138.00,97.00 Z M 170.00,101.00 C 167.61,104.89 163.46,117.04 161.69,122.00 160.68,124.85 159.42,129.42 157.35,131.57 154.35,134.70 144.63,134.97 141.31,132.26 138.72,130.15 139.57,127.81 136.69,124.00 133.67,120.01 121.17,107.98 117.00,105.00 117.00,105.00 117.00,147.00 117.00,147.00 117.00,147.00 118.00,164.00 118.00,164.00 118.00,164.00 118.00,240.00 118.00,240.00 118.00,240.00 119.00,255.00 119.00,255.00 119.00,255.00 119.00,281.00 119.00,281.00 130.12,280.97 143.79,277.97 154.00,273.57 157.41,272.10 163.91,268.87 165.83,265.68 167.04,263.66 167.98,249.10 168.04,246.00 168.04,246.00 168.04,234.00 168.04,234.00 168.04,234.00 169.00,222.00 169.00,222.00 169.00,222.00 169.00,203.00 169.00,203.00 169.00,203.00 170.00,187.00 170.00,187.00 170.00,187.00 170.00,138.00 170.00,138.00 170.00,138.00 170.96,124.00 170.96,124.00 170.96,124.00 170.96,110.00 170.96,110.00 170.96,110.00 170.00,101.00 170.00,101.00 Z M 61.00,170.00 C 61.00,170.00 26.00,166.83 26.00,166.83 23.09,166.54 14.42,166.63 16.17,161.94 17.15,159.32 26.51,149.59 28.91,147.00 28.91,147.00 57.72,115.00 57.72,115.00 62.04,110.08 67.30,102.14 74.00,101.00 74.00,101.00 67.30,119.00 67.30,119.00 67.30,119.00 57.00,142.00 57.00,142.00 57.00,142.00 87.00,142.00 87.00,142.00 87.00,142.00 105.00,143.00 105.00,143.00 103.03,149.25 89.97,169.20 85.68,176.00 85.68,176.00 52.95,229.00 52.95,229.00 52.95,229.00 41.20,248.00 41.20,248.00 38.68,252.12 38.18,254.67 33.00,254.00 33.00,254.00 46.33,213.00 46.33,213.00 46.33,213.00 61.00,170.00 61.00,170.00 Z"
+			width="200"
+			height="200"
+		/>
+
+		<g style="transform: scale(0.5) translate(450px, 50px)">
+			<g>
+				<path
+					style="transform:translate(6px,6px)"
+					d="M12 0L12.6522 9.56587L18 1.6077L13.7819 10.2181L22.3923 6L14.4341 11.3478L24 12L14.4341 12.6522L22.3923 18L13.7819 13.7819L18 22.3923L12.6522 14.4341L12 24L11.3478 14.4341L6 22.3923L10.2181 13.7819L1.6077 18L9.56587 12.6522L0 12L9.56587 11.3478L1.6077 6L10.2181 10.2181L6 1.6077L11.3478 9.56587L12 0Z"
+					fill="#ffffaa"
+				/>',
+				<animateTransform
+					attributeName="transform"
+					type="rotate"
+					from="0 18 18"
+					to="360 18 18"
+					dur="10s"
+					repeatCount="indefinite"
+				/>
+			</g>
+		</g>
+		<g style="transform: scale(0.5) translate(468px, 50px)">
+			<g>
+				<path
+					style="transform:translate(6px,6px)"
+					d="M12 0L12.6522 9.56587L18 1.6077L13.7819 10.2181L22.3923 6L14.4341 11.3478L24 12L14.4341 12.6522L22.3923 18L13.7819 13.7819L18 22.3923L12.6522 14.4341L12 24L11.3478 14.4341L6 22.3923L10.2181 13.7819L1.6077 18L9.56587 12.6522L0 12L9.56587 11.3478L1.6077 6L10.2181 10.2181L6 1.6077L11.3478 9.56587L12 0Z"
+					fill="#ffffff"
+				/>
+				<animateTransform
+					attributeName="transform"
+					type="rotate"
+					from="0 18 18"
+					to="360 18 18"
+					dur="10s"
+					repeatCount="indefinite"
+				/>
+			</g>
+		</g>
+		<g style="transform: scale(0.5) translate(486px, 50px)">
+			<g>
+				<path
+					style="transform:translate(6px,6px)"
+					d="M12 0L12.6522 9.56587L18 1.6077L13.7819 10.2181L22.3923 6L14.4341 11.3478L24 12L14.4341 12.6522L22.3923 18L13.7819 13.7819L18 22.3923L12.6522 14.4341L12 24L11.3478 14.4341L6 22.3923L10.2181 13.7819L1.6077 18L9.56587 12.6522L0 12L9.56587 11.3478L1.6077 6L10.2181 10.2181L6 1.6077L11.3478 9.56587L12 0Z"
+					fill="#77ffff"
+				/>
+				<animateTransform
+					attributeName="transform"
+					type="rotate"
+					from="0 18 18"
+					to="360 18 18"
+					dur="10s"
+					repeatCount="indefinite"
+				/>
+			</g>
+		</g>
+	</svg>`,e.replace(/[\s\n]+/g," ")}const s0=window.IpfsCore;class i0{constructor(e=void 0){const{set:l,subscribe:a,update:n}=o0(e);this.set=l,this.subscribe=a,this.update=n}get(){return K(this)}}function z(t,e,l){const a=t.slice();return a[8]=e[l][0],a[9]=e[l][1],a[10]=e,a[11]=l,a}function T(t,e,l){const a=t.slice();return a[12]=e[l],a}function r0(t){let e,l,a,n,s,o,i,d,C,B,m={ctx:t,current:null,token:null,hasCatch:!1,pending:f0,then:u0,catch:h0,value:7};D(e=N(t[1]),m);let A=Object.entries(t[2]),p=[];for(let r=0;r<A.length;r+=1)p[r]=R(z(t,A,r));return{c(){m.block.c(),l=M(),a=I("div");for(let r=0;r<p.length;r+=1)p[r].c();n=M(),s=I("p"),o=M(),i=I("button"),d=S("Upload to IPFS"),this.h()},l(r){m.block.l(r),l=x(r),a=k(r,"DIV",{class:!0});var c=v(a);for(let y=0;y<p.length;y+=1)p[y].l(c);c.forEach(h),n=x(r),s=k(r,"P",{});var _=v(s);_.forEach(h),o=x(r),i=k(r,"BUTTON",{class:!0});var u=v(i);d=H(u,"Upload to IPFS"),u.forEach(h),this.h()},h(){L(a,"class","controls svelte-16bcuil"),L(i,"class","svelte-16bcuil")},m(r,c){m.block.m(r,m.anchor=c),m.mount=()=>l.parentNode,m.anchor=l,b(r,l,c),b(r,a,c);for(let _=0;_<p.length;_+=1)p[_].m(a,null);b(r,n,c),b(r,s,c),b(r,o,c),b(r,i,c),w(i,d),C||(B=E(i,"click",t[4]),C=!0)},p(r,c){if(t=r,m.ctx=t,c&2&&e!==(e=N(t[1]))&&D(e,m)||Q(m,t,c),c&6){A=Object.entries(t[2]);let _;for(_=0;_<A.length;_+=1){const u=z(t,A,_);p[_]?p[_].p(u,c):(p[_]=R(u),p[_].c(),p[_].m(a,null))}for(;_<p.length;_+=1)p[_].d(1);p.length=A.length}},d(r){m.block.d(r),m.token=null,m=null,r&&h(l),r&&h(a),V(p,r),r&&h(n),r&&h(s),r&&h(o),r&&h(i),C=!1,B()}}}function c0(t){let e,l,a={ctx:t,current:null,token:null,hasCatch:!1,pending:p0,then:d0,catch:_0,value:7};return D(l=F(t[0]),a),{c(){e=G(),a.block.c()},l(n){e=G(),a.block.l(n)},m(n,s){b(n,e,s),a.block.m(n,a.anchor=s),a.mount=()=>e.parentNode,a.anchor=e},p(n,s){t=n,a.ctx=t,s&1&&l!==(l=F(t[0]))&&D(l,a)||Q(a,t,s)},d(n){n&&h(e),a.block.d(n),a.token=null,a=null}}}function h0(t){return{c:f,l:f,m:f,p:f,d:f}}function u0(t){let e,l=t[7]+"",a;return{c(){e=new a0,a=G(),this.h()},l(n){e=l0(n),a=G(),this.h()},h(){e.a=a},m(n,s){e.m(l,n,s),b(n,a,s)},p(n,s){s&2&&l!==(l=n[7]+"")&&e.p(l)},d(n){n&&h(a),n&&e.d()}}}function f0(t){return{c:f,l:f,m:f,p:f,d:f}}function O(t){let e,l=t[12]+"",a,n;return{c(){e=I("option"),a=S(l),this.h()},l(s){e=k(s,"OPTION",{});var o=v(e);a=H(o,l),o.forEach(h),this.h()},h(){e.__value=n=t[12],e.value=e.__value},m(s,o){b(s,e,o),w(e,a)},p:f,d(s){s&&h(e)}}}function R(t){let e,l,a=t[8]+"",n,s,o,i,d,C,B,m,A,p,r=t[9],c=[];for(let u=0;u<r.length;u+=1)c[u]=O(T(t,r,u));function _(){t[5].call(i,t[8])}return{c(){e=I("div"),l=I("label"),n=S(a),o=M(),i=I("select"),d=I("option"),C=S("None");for(let u=0;u<c.length;u+=1)c[u].c();m=M(),this.h()},l(u){e=k(u,"DIV",{class:!0});var y=v(e);l=k(y,"LABEL",{for:!0,class:!0});var g=v(l);n=H(g,a),g.forEach(h),o=x(y),i=k(y,"SELECT",{name:!0});var P=v(i);d=k(P,"OPTION",{});var Z=v(d);C=H(Z,"None"),Z.forEach(h);for(let J=0;J<c.length;J+=1)c[J].l(P);P.forEach(h),m=x(y),y.forEach(h),this.h()},h(){L(l,"for",s=t[8]),L(l,"class","svelte-16bcuil"),d.__value="",d.value=d.__value,L(i,"name",B=t[8]),t[1][t[8]]===void 0&&t0(_),L(e,"class","control svelte-16bcuil")},m(u,y){b(u,e,y),w(e,l),w(l,n),w(e,o),w(e,i),w(i,d),w(d,C);for(let g=0;g<c.length;g+=1)c[g].m(i,null);j(i,t[1][t[8]]),w(e,m),A||(p=E(i,"change",_),A=!0)},p(u,y){if(t=u,y&4){r=t[9];let g;for(g=0;g<r.length;g+=1){const P=T(t,r,g);c[g]?c[g].p(P,y):(c[g]=O(P),c[g].c(),c[g].m(i,null))}for(;g<c.length;g+=1)c[g].d(1);c.length=r.length}y&6&&j(i,t[1][t[8]])},d(u){u&&h(e),V(c,u),A=!1,p()}}}function _0(t){return{c:f,l:f,m:f,p:f,d:f}}function d0(t){let e,l,a,n,s;return{c(){e=I("img"),a=M(),n=I("span"),s=S(t[0]),this.h()},l(o){e=k(o,"IMG",{src:!0,alt:!0}),a=x(o),n=k(o,"SPAN",{class:!0});var i=v(n);s=H(i,t[0]),i.forEach(h),this.h()},h(){W(e.src,l=t[7])||L(e,"src",l),L(e,"alt",""),L(n,"class","metedata-url svelte-16bcuil")},m(o,i){b(o,e,i),b(o,a,i),b(o,n,i),w(n,s)},p(o,i){i&1&&!W(e.src,l=o[7])&&L(e,"src",l),i&1&&n0(s,o[0])},d(o){o&&h(e),o&&h(a),o&&h(n)}}}function p0(t){return{c:f,l:f,m:f,p:f,d:f}}function g0(t){let e,l;function a(o,i){return o[0]?c0:r0}let n=a(t),s=n(t);return{c(){e=M(),l=I("section"),s.c(),this.h()},l(o){$('[data-svelte="svelte-x44elq"]',document.head).forEach(h),e=x(o),l=k(o,"SECTION",{class:!0});var d=v(l);s.l(d),d.forEach(h),this.h()},h(){document.title="BannyVerse Forge",L(l,"class","svelte-16bcuil")},m(o,i){b(o,e,i),b(o,l,i),s.m(l,null)},p(o,[i]){n===(n=a(o))&&s?s.p(o,i):(s.d(1),s=n(o),s&&(s.c(),s.m(l,null)))},i:f,o:f,d(o){o&&h(e),o&&h(l),s.d()}}}const C0=!0;async function F(t){return(await(await fetch(t)).json()).image}function m0(t,e,l){let a;const n={Body:["Green","Pink","Red","Yellow"],Choker:["Choker","Christmas_Lights","No_Choker"],Face:["Alien","Buddha_Face","Bunny_Eyes","Eye_Mouth","Harley_Quinn","No_Face","Peach_Face","Wonder_Woman"],Headgear:["Assassin_Headgear","Cowboy_Hat","Green_Cap","Kill_Bill","Pirate","Sakura","Viking","Astronaut_Helmet","Cyberpunk_Glasses","Hannible_Lector","Mario","Playboy","Samurai_Ribbon","Witchcraft","Batman","Dorthy","Harley_Quinn","Narutohead","Princess_Leia","Shining","Wonder_Woman","Blondie","Elf","Harry_Potter","No_Hat","Professor","Smoking_Glasses","Boba","Farmer","Headphones","Obiwan","Raggae","Spock","Brown_Hair","Feather_Hat","Ironman_Helmet","Peach","Rave_Glasses","The_Mask_Hat","Chef_Hat","Fisherman","Jesus","Pharaoh","Red_Hair","Tinkerbell","Clown","Geisha","Jinx_Hair","Pink_Hat","Robinhood_Hat","Vampire"],Left_Hand:["Cyberpunk_Weapon","Holy_Wine","Nothing","Pirate_Sword","Pitchfork","Samurai_Katana","Shark_v2","Surf_Board","Viking_Shield"],Lower_Accessory:["Black_Shoes","Christmas_Boots","No_Shoes","Robinhood_Boots","Sandals","Wonder_Shoes"],Oral_Fixation:["Mouthstraw","Nothing"],Outfit:["Assasin","Dolly","Harry_Potter","Pharaoh","Robinhood","The_Mask","Astronaut","Dorthy","Iron_Man","Pink_Dress","Sakura","Tinkerbell","Banana_Givi","Dr.Manhattan","Jesus","Pink_Girl","Samurai","Vampire","Batman","Elf","Jinx","Pirate","Satan","Vampire_Girl","Boba_Fett","Farmer","Kill_Bill","Playboy","Shiningbody","Witchbelt","Chef","Fisherman_Vest","Mario","Princess_Leia","Smalls","Wonder_Woman_Dress","Chokha","Geisha","Musketeer","Princess_Peach","Smoking","Cowboy","Guardians_of_Gaalaxy's_Gamora","Naruto","Professor","Spock","Cyberpunk","Hannible_Lector","No_Outfit","Punk","Surfer_T-shirt","Deadpool","Harley_Quinn","Obiwan","Rave","Tao_of_Banana_Buddha_Robe"],Right_Hand:["Anch","Dagger","Harley","Lollipop","Pistol","Wagasa","Butcher_Knife","Dorthy_Basket","Katana","Musketeer_Rapier","Robinhood_Dagger","Witchbroom","Christmas_Lights","Fishing_Pole","Lightsaber","Nothing","Viking_Axe","Wonder_Woman"],Background:["1000_Days","100_Days","10_Days","500_Days","50_Days"]},s=new i0({Background:"",Body:"",Choker:"",Face:"",Headgear:"",Left_Hand:"",Lower_Accessory:"",Oral_Fixation:"",Outfit:"",Right_Hand:""});X(t,s,C=>l(1,a=C));let o="";async function i(){const C=await s0.create({repo:"ipfs-"+Math.random()}),{cid:B}=await C.add(JSON.stringify({name:`Juicebox #${1}`,attributes:[{trait_type:"Color",value:"White"},{trait_type:"Distortion Scale",value:11},{trait_type:"Rings",value:6},{trait_type:"Frequency Multiple",value:2}],description:"Distortion is a fully hand-typed 100% on-chain art collection.",image:`data:image/svg+xml;base64,${btoa(await N(a))}`}));l(0,o=`https://cloudflare-ipfs.com/ipfs/${B.toString()}`),console.log(o)}function d(C){a[C]=e0(this),s.set(a),l(2,n)}return[o,a,n,s,i,d]}class I0 extends U{constructor(e){super(),Y(this,e,m0,g0,q,{})}}export{I0 as default,C0 as prerender};
