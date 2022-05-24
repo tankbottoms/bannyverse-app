a28389e864d87759a7c317fe5de4bc770eb338f6
Evie <evita@stenqvist.co>
2022-05-24 12:31:36 +0200

    Landing title centered on black background

--- src/lib/Composer/Landing.svelte
+++ src/lib/Composer/Landing.svelte
@@ -3,12 +3,14 @@
 </section>
 
 <style>
-    section {
-        background: black;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-    }
+	section {
+		background: black;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100vw;
+		text-align: center;
+		margin-top: 20px;
+	}
 </style>
