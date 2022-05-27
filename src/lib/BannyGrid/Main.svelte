8f8724b4132fd62a1542e86a9a6012f7c34e0c0e
Evie <evita@stenqvist.co>
2022-05-27 19:01:14 +0200

    Motto overlay on Banny in grid

--- src/lib/BannyGrid/Main.svelte
+++ src/lib/BannyGrid/Main.svelte
@@ -5,6 +5,7 @@
 	import { onMount } from 'svelte';
 
 	let bannies = [];
+	let hoveredImage: string;
 
 	/**
 	 * Takes a gif string of form "bannible-lector.gif" and returns "Bannible_Lector"
@@ -23,6 +24,10 @@
 		return name.charAt(0).toUpperCase() + name.slice(1);
 	}
 
+	function onImageMouseover(bannie) {
+		hoveredImage = bannie.name;
+	}
+
 	onMount(() => {
 		gifs.forEach((gif) => {
 			const name = nameFromGif(gif);
@@ -35,13 +40,25 @@
 			}
 		});
 	});
-
-	console.log(gifs);
 </script>
 
 <section class="grid">
 	{#each bannies as bannie}
-		<img src={bannie.src} alt={bannie.name} id={bannie.name} />
+		<div class="bannieContainer" id={bannie.name}>
+			<img
+				class:active={hoveredImage === bannie.name}
+				src={bannie.src}
+				alt={bannie.name}
+				on:mouseover={() => onImageMouseover(bannie)}
+				on:focus={() => onImageMouseover(bannie)}
+			/>
+			{#if hoveredImage === bannie.name}
+				<div class="overlay">
+					<h1>{bannie.name.replaceAll('_', ' ')}</h1>
+					<p>{bannie.motto}</p>
+				</div>
+			{/if}
+		</div>
 	{/each}
 </section>
 
@@ -52,6 +69,40 @@
 		object-fit: cover;
 		border-radius: 5%;
 	}
+
+	.bannieContainer {
+		position: relative;
+	}
+
+	h1,
+	p {
+		color: white;
+		padding: 0rem 1rem;
+	}
+
+	h1 {
+		/* TODO the correct motto */
+		font-family: Arial, Helvetica, sans-serif;
+		font-weight: 700;
+		line-height: 1;
+	}
+
+	/* Create an image overlay */
+	.overlay {
+		position: absolute;
+		height: 100%;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background: rgba(0, 0, 0, 0.5);
+		color: #fff;
+		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
 	.grid {
 		max-width: 1180px;
 		margin: 0 auto;
@@ -62,7 +113,8 @@
 		grid-auto-flow: dense;
 	}
 
-	.grid > img:nth-child(2), #Ivar_the_Boneless {
+	.grid > .bannieContainer:nth-child(2),
+	#Ivar_the_Boneless {
 		grid-column: span 2; /* Spans two columns */
 		grid-row: span 2; /* Spans two rows */
 	}
@@ -72,18 +124,22 @@
 		grid-row: span 1;
 	}
 
-	.grid > img:nth-child(3) {
-		grid-column: span 1; /* Spans two columns */
-		grid-row: span 1; /* Spans two rows */
-	}
-	/* This will create 2x images every 3 elements */
-	.grid > img:nth-child(3n) {
-		grid-column: span 1; /* Spans two columns */
-		grid-row: span 2; /* Spans two rows */
+	#Ivar_the_Boneless {
+		grid-column: span 2;
+		grid-row: span 2;
 	}
 
-	/* This will create 3x images every 5 elements */
-	.grid > img:nth-child(5n) {
+	.grid > .bannieContainer:nth-child(3) {
+		grid-column: span 1;
+		grid-row: span 1;
+	}
+	/* This will create 2x images every 3 elements */
+	.grid > .bannieContainer:nth-child(3n) {
+		grid-column: span 1;
+		grid-row: span 2;
+	}
+
+	.grid > .bannieContainer:nth-child(5n) {
 		grid-column: span 2;
 		grid-row: span 3;
 	}
