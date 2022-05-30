014c4f740e5d42b01dca236b00580331b00ad97e
Evie <evita@stenqvist.co>
2022-05-30 14:58:52 +0200

    Declare Banny type as global type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,12 @@
+import characters from '$data/characters.json';
+
 /// <reference types="@sveltejs/kit" />
 
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
-declare namespace App {}
+declare namespace App {
+}
+
+declare global {
+    type Banny = typeof characters[0];
+}
