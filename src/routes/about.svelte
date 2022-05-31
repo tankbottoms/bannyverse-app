bc441ec528633f0b06c7fef7ceabedfc70c9843d
Evie <evita@stenqvist.co>
2022-05-31 11:36:12 +0200

    About placeholder with todo's as comments

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,4 @@
 <script context="module" lang="ts">
-
 </script>
 
 <svelte:head>
@@ -7,17 +6,12 @@
 </svelte:head>
 
 <div class="content">
-	<h1>About this app</h1>
+	<h1>About</h1>
+	<img width="200" src="composer/great.png" alt="Banny thumbs up" />
+	<p>More info on the project and the team coming soon...</p>
 
-	<p>
-		The BannyVerse origin story.
-	</p>	
-	<p>
-		Purpose of the treasury.
-	</p>
-
-	<p>		
-	</p>
+	<!-- TODO The BannyVerse origin story -->
+	<!-- TODO Purpose of the treasury -->
 </div>
 
 <style>
@@ -25,5 +19,22 @@
 		width: 100%;
 		max-width: var(--column-width);
 		margin: var(--column-margin-top) auto 0 auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	h1 {
+		font-size: 10vw;
+		color: white;
+		margin-bottom: 0;
+		transform-origin: 50% 100%;
+		transform: perspective(150px) rotateX(20deg);
+	}
+
+	p {
+		color: white;
+		text-align: center;
+		font-weight: 300;
 	}
 </style>
