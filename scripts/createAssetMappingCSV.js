dd997274717cece62ee46b91e6cdcf29338b59f0
Evie <evita@stenqvist.co>
2022-06-26 18:16:11 +0200

    Fix the unique assets to csv script

--- scripts/createAssetMappingCSV.js
+++ scripts/createAssetMappingCSV.js
@@ -7,6 +7,30 @@ import charactersJson from '../src/data/characters.json' assert { type: 'json' }
  */
 let data = {};
 
+function getDirectories(srcpath) {
+	return fs.readdirSync(srcpath).filter(file => fs.lstatSync(`${srcpath}/${file}`).isDirectory());
+}
+
+function addOtherAssets() {
+    // We need to add the "Other" asset options to the layerOptions
+	const otherDirectory =
+    './docs/veBanny-rebuild-assets/veBanny_Final_Non-Standard_Character_Accessory/OTHER_non-standard_accessories';
+
+    const characterLayers = getDirectories(otherDirectory);
+    characterLayers.forEach(layer => {
+        const assets = fs.readdirSync(`${otherDirectory}/${layer}`);
+        assets.forEach(asset => {
+            let name = asset.split('.')[0];
+            name = `${name}_${layer}`;
+            data[name] = {
+                layer,
+                character: "OTHER",
+                characterIndex: 77,
+            }
+        });
+    });
+}
+
 Object.keys(charactersJson).forEach(characterIndex => {
     const character = charactersJson[characterIndex];
     const metadata = character.metadata;
@@ -17,8 +41,12 @@ Object.keys(charactersJson).forEach(characterIndex => {
         // Check if asset already exists on data, create if not
         // As we are iterating over incrementing Bannies, we'll use the character with the lowest index
         // as the asset metadata.
-        if (!data[asset]) {
-            data[asset] = {
+        const assetName = `${asset}_${layer}`;
+
+        let hasAsset = data[assetName];
+
+        if (!hasAsset) {
+            data[assetName] = {
                 layer,
                 character: metadata.name,
                 characterIndex,
@@ -33,6 +61,8 @@ Object.keys(charactersJson).forEach(characterIndex => {
         }
     }
 })
+
+addOtherAssets()
 
 // Create header row
 const columns = ['asset', ...Object.keys(data[Object.keys(data)[0]])].join(', ');
